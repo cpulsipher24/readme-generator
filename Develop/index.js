5bd16ac1b8fe955de35fcdc558cc1092248e7ae4
Cpulsipher24 <collintpulsipher@gmail.com>
2024-01-20 16:46:00 -0700

    Added first two questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,19 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-// TODO: Create an array of questions for user input
-const questions = [];
-
+// Array of questions for user input
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a short description of your project:',
+    },
+    // Add more questions for other sections like Installation, Usage, License, etc.
+  ];
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
