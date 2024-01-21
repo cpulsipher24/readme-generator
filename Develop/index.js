a8ac4a8e34cbe9a40b5dfe1b15e765501eaf3bc0
Cpulsipher24 <collintpulsipher@gmail.com>
2024-01-20 17:12:12 -0700

    Added more questions and sections for readme file

--- Develop/index.js
+++ Develop/index.js
@@ -1,44 +1,104 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
 // Array of questions for user input
 const questions = [
-    {
-      type: 'input',
-      name: 'title',
-      message: 'What is the title of your project?',
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'Provide a short description of your project:',
-    },
-    // Add more questions for other sections like Installation, Usage, License, etc.
-  ];
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a short description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'How do users install your application?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'How should users use your application?',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your application:',
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'How can users contribute to your project?',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide instructions for running tests:',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
+];
+
 // Function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) =>
-      err ? console.error(err) : console.log('README.md generated successfully!')
-    );
-  }
-  
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log('README.md generated successfully!')
+  );
+}
+
 // Function to initialize app
 function init() {
-    inquirer.prompt(questions).then((answers) => {
-      // Use the answers to dynamically generate the content of the README
-      const readmeContent = `
-  # ${answers.title}
-  
-  ## Description
-  ${answers.description}
-  
-  // Add more sections based on user input
-  
-  `;
-  
-      // Call the writeToFile function to create the README file
-      writeToFile('README.md', readmeContent);
-    });
-  }
-  
-  // Function call to initialize app
-  init();
+  inquirer.prompt(questions).then((answers) => {
+    // Generate content for the README based on user input
+    const readmeContent = `
+# ${answers.title}
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+This project is licensed under the ${answers.license} license.
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions
+For questions or concerns, please contact me through my [GitHub profile](https://github.com/${answers.github}) or via email at ${answers.email}.
+`;
+
+    // Call the writeToFile function to create the README file
+    writeToFile('README.md', readmeContent);
+  });
+}
+
+// Function call to initialize app
+init();
